6b709f926d7ad63e434ff351f60931dca742184a
Denis <denisxor@gmail.com>
2020-03-02 22:24:23 +0300

    added Context implementation

--- src/utils/errors.rs
+++ src/utils/errors.rs
@@ -19,6 +19,7 @@ use std::fmt::Display;
 
 #[derive(Debug)]
 pub enum AeronError {
+    GenericError(String),
     IllegalArgumentException(String),
     IllegalStateException(String),
 }
@@ -26,6 +27,7 @@ pub enum AeronError {
 impl Display for AeronError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
+            AeronError::GenericError(msg) => write!(f, "Aeron error: {}", msg),
             AeronError::IllegalArgumentException(msg) => write!(f, "Illegal argument: {}", msg),
             AeronError::IllegalStateException(msg) => write!(f, "Illegal state: {}", msg),
         }
