dffb6bb6244e93288e8722f92f973d65713ae656
Kirill Karbushev <KiriosK@yandex.ru>
2020-11-30 13:10:15 +0300

    remove unnecessary pattern matching in throughput example

--- examples/throughput.rs
+++ examples/throughput.rs
@@ -256,20 +256,13 @@ fn main() {
 
             offer_idle_strategy.reset();
 
-            loop {
-                match publication
-                    .lock()
-                    .unwrap()
-                    .try_claim(settings.message_length, &mut buffer_claim)
-                {
-                    Ok(_) => break,
-                    Err(AeronError::BackPressured) => {
-                        back_pressure_count += 1;
-                        offer_idle_strategy.idle();
-                    }
-                    // TODO: Decide, if we need to keep stats for other errors
-                    _ => continue,
-                }
+            while let Err(AeronError::BackPressured) = publication
+                .lock()
+                .unwrap()
+                .try_claim(settings.message_length, &mut buffer_claim)
+            {
+                back_pressure_count += 1;
+                offer_idle_strategy.idle();
             }
 
             buffer_claim.buffer().put::<i64>(buffer_claim.offset(), i);
